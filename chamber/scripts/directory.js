cfceff0c911c2817718141e4478617f4c384b335
lucioboccazzi <lucioboccazzi@gmail.com>
2024-11-14 07:54:15 -0300

    part 1

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,7 @@
+// Getting dates
+const year = document.querySelector("#year");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+var lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
+
