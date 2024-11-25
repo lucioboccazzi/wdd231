34004151bad171a8be9a4a6fb0912ffaada15297
lucioboccazzi <lucioboccazzi@gmail.com>
2024-11-25 07:48:20 -0300

    base done

---
+++ chamber/scripts/join.js
@@ -0,0 +1,23 @@
+// Getting dates
+const year = document.querySelector("#year");
+const today = new Date();
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+var lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastModified;
+
+
+//Modals
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        closeModal(event.target.id);
+    }
+};
