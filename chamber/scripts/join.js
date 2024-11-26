3260b8bfa022071639ab26367f71c8a2d55aeb29
lucioboccazzi <lucioboccazzi@gmail.com>
2024-11-26 07:43:29 -0300

    s

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -5,32 +5,29 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 var lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = lastModified;
 
+
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('membership-form');
 
-    // Set timestamp dynamically
     const timestampField = document.getElementById('timestamp');
-    timestampField.value = today.toISOString(); // Sets the timestamp to current time
+    timestampField.value = today.toISOString();
 
     if (form) {
         form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Prevent the form from submitting normally
+            event.preventDefault();
 
-            // Get form data
-            const formData = new FormData(this); // Collect form data
-            const queryString = new URLSearchParams(formData).toString(); // Convert form data to query string
+            const formData = new FormData(this); 
+            const queryString = new URLSearchParams(formData).toString(); 
 
-            // Redirect to thankyou.html with query parameters
             window.location.href = 'thankyou.html?' + encodeURIComponent(queryString);
         });
     }
 });
 
-// Extract query parameters and display on thank you page
+
 if (window.location.pathname.includes('thankyou.html')) {
     const params = new URLSearchParams(decodeURIComponent(window.location.search));
 
-    // Update the page content with the query parameters
     document.getElementById('first-name').textContent = params.get('first-name');
     document.getElementById('last-name').textContent = params.get('last-name');
     document.getElementById('email').textContent = params.get('email');
@@ -41,7 +38,6 @@ if (window.location.pathname.includes('thankyou.html')) {
     document.getElementById('timestamp').textContent = params.get('timestamp');
 }
 
-// Modals
 function openModal(modalId) {
     document.getElementById(modalId).style.display = "block";
 }
