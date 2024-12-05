98926db84ab6500c1ecd496f5435790e2a7ae452
lucioboccazzi <lucioboccazzi@gmail.com>
2024-12-05 07:31:27 -0300

    done?

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -2,6 +2,37 @@
 const year = document.querySelector("#year");
 const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
 var lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = lastModified;
+
+
+// Function to for visit message
+function displayVisitMessage() {
+    const lastVisit = localStorage.getItem('lastVisit');
+    const currentDate = new Date();
+    const visitMessageElement = document.getElementById('visitMessage');
+    
+    if (!lastVisit) {
+
+        visitMessageElement.textContent = "Welcome! Let us know if you have any questions.";
+    } else {
+        const lastVisitDate = new Date(lastVisit);
+        const timeDiff = currentDate - lastVisitDate;
+        const daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+        
+        if (daysDiff < 1) {
+
+            visitMessageElement.textContent = "Back so soon! Awesome!";
+        } else {
+
+            const dayWord = daysDiff === 1 ? "day" : "days";
+            visitMessageElement.textContent = `You last visited ${daysDiff} ${dayWord} ago.`;
+        }
+    }
+
+    localStorage.setItem('lastVisit', currentDate.toISOString());
+}
+
+displayVisitMessage();
 
