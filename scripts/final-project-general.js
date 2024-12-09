0c484f7c17b4b415fcba833b39e6d699d3a43c6c
lucioboccazzi <lucioboccazzi@gmail.com>
2024-12-09 14:42:06 -0300

    S

--- scripts/final-project-general.js
+++ scripts/final-project-general.js
@@ -5,13 +5,3 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 var lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = lastModified;
 
-
-
-const hamburgerIcon = document.getElementById('hamburger-icon');
-const navLinks = document.getElementById('nav-links');
-
-// Toggle the 'active' class on the navLinks and hamburger icon
-hamburgerIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    hamburgerIcon.classList.toggle('active');
-});
